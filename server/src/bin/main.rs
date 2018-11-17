1e62b2b0aa0894756aa2b4659d49712e560da202
Vikas Sahu <vikassahu2904@gmail.com>
2018-11-17 03:12:19 -0600

    Add request parting and persistent state

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -1,25 +1,102 @@
 extern crate iron;
 extern crate server;
 extern crate router;
+extern crate persistent;
+extern crate params;
 
 use iron::prelude::*;
 use iron::status;
 use router::Router;
 use server::*;
+use server::models::*;
+use persistent::Write;
+use iron::typemap::Key;
+
+#[derive(Copy, Clone)]
+pub struct Data;
+impl Key for Data { type Value = application::Application; }
+
+fn register_handler(req: &mut Request) -> IronResult<Response> {
+    let mutex = req.get::<Write<Data>>().unwrap();
+    let mut app = mutex.lock().unwrap();
+    use params::{Params, Value};
+
+    let map = req.get_ref::<Params>().unwrap();
+    
+    let username: String;
+    match map.get("username") {
+        Some(&Value::String(ref u)) => username = u.to_string(),
+        _ => {username = String::new()}
+    };
+
+    let password: String;
+    
+    match map.get("password") {
+        Some(&Value::String(ref p)) => {
+            password = p.to_string();
+        }
+        _ => { password = String::new() }
+    };
+
+    let user = User{
+        username,
+        password
+    };
+
+    let res = accounts::register_user(&mut app, &user);
+    match res {
+        Ok(_) => Ok(Response::with((status::Created,"Okay"))),
+        Err(s) => Ok(Response::with((status::BadRequest, s)))
+    }
+}
+
+fn login_handler(req: &mut Request) -> IronResult<Response> {
+    let mutex = req.get::<Write<Data>>().unwrap();
+    let mut app = mutex.lock().unwrap();
+    use params::{Params, Value};
+
+    let map = req.get_ref::<Params>().unwrap();
+    
+    let username: String;
+    match map.get("username") {
+        Some(&Value::String(ref u)) => username = u.to_string(),
+        _ => {username = String::new()}
+    };
+
+    let password: String;
+    
+    match map.get("password") {
+        Some(&Value::String(ref p)) => {
+            password = p.to_string();
+        }
+        _ => { password = String::new() }
+    };
+
+    let user = User{
+        username,
+        password
+    };
+
+    let res = accounts::login_user(&mut app, &user);
+    match res {
+        Ok(_) => Ok(Response::with((status::Ok,"Okay"))),
+        Err(s) => Ok(Response::with((status::BadRequest, s)))
+    }
+}
 
 
 fn main() {
-    let _application = application::init();
+    let application = application::init();
     let mut router = Router::new();           // Alternative syntax:
-    router.get("/", handler, "index");        // let router = router!(index: get "/" => handler,
-    router.get("/:query", handler, "query");  //                      query: get "/:query" => handler);
-    Iron::new(router)
+    router.post("/register", register_handler, "register");
+    router.post("/login", login_handler, "login");
+    let mut chain = Chain::new(router);
+    chain.link(Write::<Data>::both(application));
+
+    // router.get("/", handler(&mut application), "index");        // let router = router!(index: get "/" => handler,
+    // router.get("/:query", handler(&mut application), "query");  //                      query: get "/:query" => handler);
+    Iron::new(chain)
         .http("0.0.0.0:3000").unwrap();
     
     
-}
-
-fn handler(req: &mut Request) -> IronResult<Response> {
-        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
-        Ok(Response::with((status::Ok, *query)))
 }
